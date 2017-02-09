9ccb70c8d387b9ace5c3e0071ff8ae93328b1c6e
jvargasipince <jvargas.ipince@gmail.com>
2017-02-08 21:28:52 -0500

    Add project files.

---
+++ LazyLoad_Demo/LoadData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LazyLoad_Demo
+{
+    public class LoadData
+    {
+       public static void Main(string[] args)
+        {
+
+            LoadDataAlmacen();
+
+        }
+
+        static void LoadDataAlmacen()
+        {
+            var Empresas_AlmacenesEntities = new AlmacenEntities();
+
+            //Instanciamos solo la entidad Empresa
+            var Empresas = Empresas_AlmacenesEntities.Empresa.Include("Almacen");  
+
+            foreach (var empresa in Empresas)
+            {
+
+                Console.WriteLine("La empresa obtenida es : {0} y su fecha de creación es {1}", empresa.nombre, empresa.fecha.ToString());
+
+                //Listamos sus almacenes por cada empresa
+
+                foreach (var almacen in empresa.Almacen)
+                {
+                    Console.WriteLine("El almacen para la empresa seleccionada  es : {0} y su fecha de creación es {1}", almacen.nombre, almacen.fecha.ToString());
+                }
+
+                Console.WriteLine("Pulse una tecla para continuar");
+                Console.ReadKey();
+            }
+
+        }   
+
+
+    }
+}
